782cf1471fa9008bad4ef4006361cb4b0e4fb359
Spomega <spomegasys@gmail.com>
2019-12-22 23:23:00 +0000

    adding admin transaction filters

--- resources/views/backend/auth/transaction/adminindex.blade.php
+++ resources/views/backend/auth/transaction/adminindex.blade.php
@@ -58,7 +58,7 @@
                                     <td>{{ucwords($transaction->total_cost)}}</td>
                                     <td>{{ucwords($transaction->phone_number)}}</td>
                                     <td>{{ucwords($transaction->date_paid)}}</td>
-                                    <td>{{ucwords($transaction->user->full_name)}}</td>
+                                    <td>{{ucwords($transaction->user['full_name'])}}</td>
                                 </tr>
                             @endforeach
                             </tbody>
